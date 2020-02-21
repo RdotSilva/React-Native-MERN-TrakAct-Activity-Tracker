66d9c24b9d546701011e7d933a93708ff92b76b4
RdotSilva <RyandotSilva@gmail.com>
2020-02-21 12:05:38 -0500

    Create new user inside of signup route

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -6,6 +6,12 @@ const User = mongoose.model("User");
 const router = express.Router();
 
 router.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = new User({ email, password });
+
+  user.save();
+
   res.send("Post req made to signup route");
 });
 

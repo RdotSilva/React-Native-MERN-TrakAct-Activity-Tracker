52dd715815454e406ae139fc1acefdf8fbe96726
RdotSilva <RyandotSilva@gmail.com>
2020-02-22 12:30:05 -0500

    Add signin route

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -21,4 +21,18 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must supply email and password" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+});
+
 module.exports = router;

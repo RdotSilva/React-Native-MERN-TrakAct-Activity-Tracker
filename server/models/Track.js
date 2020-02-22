4e2cd3793966a69db6f2d011ea8152a8ee952bca
RdotSilva <RyandotSilva@gmail.com>
2020-02-22 12:59:10 -0500

    Add trackSchema

---
+++ server/models/Track.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const trackSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  name: {
+    type: String,
+    default: ""
+  },
+  locations: [pointSchema]
+});

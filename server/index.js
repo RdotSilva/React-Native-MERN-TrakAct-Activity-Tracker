be7630441cee395909fc44c6404c834cded3e185
RdotSilva <RyandotSilva@gmail.com>
2020-02-22 12:08:23 -0500

    Add auth middleware to get route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const mongoURI = require("./config/config").mongoURI;
 const bodyParser = require("body-parser");
+const requireAuth = require("./middleware/requireAuth");
 
 // Routes
 const auth = require("./routes/auth/auth");
@@ -25,8 +26,8 @@ mongoose.connection.on("error", err => {
   console.error(`Error connected to MongoDB: ${err}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {

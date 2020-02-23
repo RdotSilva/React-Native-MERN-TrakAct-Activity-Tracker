73bf84449ac2d42a173377827553b85a9013de97
RdotSilva <RyandotSilva@gmail.com>
2020-02-23 10:41:37 -0500

    Add getAllTracks route

---
+++ server/routes/auth/track.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const requireAuth = require("../middleware/requireAuth");
+const { getAllTracks } = require("../../controllers/track");
+const router = express.Router();
+
+router.get("/", requireAuth, getAllTracks);
+
+module.exports = router;

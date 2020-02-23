21f55fcaff5098ae67422bdd29507994ee1d1b47
RdotSilva <RyandotSilva@gmail.com>
2020-02-23 11:47:54 -0500

    Add stack navigator

--- client/App.js
+++ client/App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AccountScreen from "./src/screens/AccountScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 
-export default function App() {
+// React navigation
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isLoggedIn ? (
+          <>
+            <Stack.Screen
+              name="TrackList"
+              component={TrackListScreen}
+              options={{ title: "Track List" }}
+            />
+            <Stack.Screen
+              name="TrackDetail"
+              component={TrackDetailScreen}
+              options={{ title: "Track Details" }}
+            />
+          </>
+        ) : (
+          <Stack.Screen
+            name="SignIn"
+            component={SigninScreen}
+            options={{ title: "Sign In" }}
+          />
+        )}
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const App = () => {
+  return (
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});

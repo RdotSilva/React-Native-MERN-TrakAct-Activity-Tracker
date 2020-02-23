b16bfc3c4ed777f21463fe7997bbf83caf31c8a5
RdotSilva <RyandotSilva@gmail.com>
2020-02-23 11:00:23 -0500

    Add create track route

--- server/routes/track/track.js
+++ server/routes/track/track.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const requireAuth = require("../../middleware/requireAuth");
-const { getAllTracks } = require("../../controllers/track");
+const { getAllTracks, createTrack } = require("../../controllers/track");
 const router = express.Router();
 
 router.get("/", requireAuth, getAllTracks);
+router.post("/", requireAuth, createTrack);
 
 module.exports = router;

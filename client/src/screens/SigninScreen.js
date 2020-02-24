65c65b816ecba6ef7426f5e35911c53ac65886d5
RdotSilva <RyandotSilva@gmail.com>
2020-02-24 10:36:54 -0500

    Add button to goto signup

--- client/src/screens/SigninScreen.js
+++ client/src/screens/SigninScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const SigninScreen = () => {
+const SigninScreen = ({ navigation }) => {
   return (
     <View>
       <Text>SigninScreen</Text>
+      <Button
+        title="Go to Signup"
+        onPress={() => navigation.navigate("Signup")}
+      ></Button>
     </View>
   );
 };

e8085644799c23ee01f088dd371c0bbe919f3cf0
RdotSilva <RyandotSilva@gmail.com>
2020-02-26 13:32:57 -0500

    Add AuthForm component

---
+++ client/src/components/Auth/AuthForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+
+const AuthForm = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AuthForm;

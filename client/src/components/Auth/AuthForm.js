215b507423c8158e67ca94e0656ab07afc3cb494
RdotSilva <RyandotSilva@gmail.com>
2020-02-26 13:33:57 -0500

    Add state

--- client/src/components/Auth/AuthForm.js
+++ client/src/components/Auth/AuthForm.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 
 const AuthForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View>
       <Text></Text>

44a0ce5d165c5af4b8492919555cd92757c42639
RdotSilva <RyandotSilva@gmail.com>
2020-02-26 11:47:25 -0500

    Refactor signUp action

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -18,24 +18,21 @@ const authReducer = (state, action) => {
   }
 };
 
-const signUp = dispatch => {
-  return async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post("/api/v1/auth/signup", {
-        email,
-        password
-      });
-      await AsyncStorage.setItem("token", response.data.token);
-      dispatch({ type: "SIGN_UP", payload: response.data.token });
-    } catch (err) {
-      dispatch({
-        type: "ADD_ERROR",
-        payload: "Something went wrong with sign up"
-      });
-    }
-  };
+const signUp = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/api/v1/auth/signup", {
+      email,
+      password
+    });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "SIGN_UP", payload: response.data.token });
+  } catch (err) {
+    dispatch({
+      type: "ADD_ERROR",
+      payload: "Something went wrong with sign up"
+    });
+  }
 };
-
 const signIn = dispatch => {
   return ({ email, password }) => {};
 };

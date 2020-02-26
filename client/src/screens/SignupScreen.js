54c9c1d26108da2cebd2740792852880c95852ff
RdotSilva <RyandotSilva@gmail.com>
2020-02-26 13:39:43 -0500

    Add TODO

--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -6,39 +6,10 @@ import { Context as AuthContext } from "../context/authContext";
 
 const SignupScreen = ({ navigation }) => {
   const { state, signUp } = useContext(AuthContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
+    // TODO: Add AuthForm component and pass props in: headerText, errorMessage, onSubmit, submitButtonText
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up</Text>
-      </Spacer>
-      <Spacer>
-        <Input
-          label="Email"
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-      </Spacer>
-      <Spacer>
-        <Input
-          label="Password"
-          value={password}
-          onChangeText={setPassword}
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry
-        />
-      </Spacer>
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signUp({ email, password })} />
-      </Spacer>
       <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
         <Spacer>
           <Text style={styles.signInLink}>

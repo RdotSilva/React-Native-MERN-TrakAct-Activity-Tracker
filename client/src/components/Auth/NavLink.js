d71a42be458c713986302cdb0421ead268bbe383
RdotSilva <RyandotSilva@gmail.com>
2020-02-27 09:19:51 -0500

    Add NavLink component

---
+++ client/src/components/Auth/NavLink.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Spacer from "./../Spacer";
+
+const NavLink = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default NavLink;

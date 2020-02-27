013e4af92e563cb8c79bca6f2d2ef1756868d4de
RdotSilva <RyandotSilva@gmail.com>
2020-02-27 09:16:03 -0500

    Clean up imports

--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
+import { Text } from "react-native-elements";
 import Spacer from "./../components/Spacer";
 import { Context as AuthContext } from "../context/authContext";
 import AuthForm from "./../components/Auth/AuthForm";

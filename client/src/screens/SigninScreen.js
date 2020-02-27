549acb0756e52b6db49ec17cfb7cd5ad3a96d85a
RdotSilva <RyandotSilva@gmail.com>
2020-02-27 09:36:46 -0500

    Replace hard coded values with components

--- client/src/screens/SigninScreen.js
+++ client/src/screens/SigninScreen.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { Context as AuthContext } from "../context/authContext";
+import AuthForm from "./../components/Auth/AuthForm";
+import NavLink from "../components/Auth/NavLink";
 
 const SigninScreen = ({ navigation }) => {
+  const { state, signIn } = useContext(AuthContext);
+
   return (
     <View>
-      <Text>SigninScreen</Text>
-      <Button
-        title="Go to Signup"
-        onPress={() => navigation.navigate("Signup")}
-      ></Button>
+      <AuthForm
+        headerText="Sign In to TrakAct"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign In"
+        onSubmit={({ email, password }) => signIn({ email, password })}
+      />
+      <NavLink
+        linkText="Don't have an account? Sign Up"
+        routeNavName="Signup"
+      />
     </View>
   );
 };

a93a20259c7e78d74daccbf6d547a80c6296cfc0
RdotSilva <RyandotSilva@gmail.com>
2020-02-27 09:22:01 -0500

    Replace hard coded values with prop values

--- client/src/components/Auth/NavLink.js
+++ client/src/components/Auth/NavLink.js
@@ -3,13 +3,15 @@ import { TouchableOpacity, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Spacer from "./../Spacer";
 
-const NavLink = () => {
+const NavLink = ({ linkText, routeNavName }) => {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity onPress={() => navigation.navigate(routeNavName)}>
+      <Spacer>
+        <Text style={styles.signInLink}>{linkText}</Text>
+      </Spacer>
+    </TouchableOpacity>
   );
 };
 

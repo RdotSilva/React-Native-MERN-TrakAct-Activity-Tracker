d166feff38dfd386bd249cd938efd6fdab7001d3
RdotSilva <RyandotSilva@gmail.com>
2020-02-28 10:37:32 -0500

    Add SafeAreaView for account screen

--- client/src/screens/AccountScreen.js
+++ client/src/screens/AccountScreen.js
@@ -3,16 +3,17 @@ import { View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-elements";
 import Spacer from "./../components/Spacer";
 import { Context as AuthContext } from "../context/authContext";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const AccountScreen = () => {
   const { signOut } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView>
       <Text>AccountScreen</Text>
       <Spacer>
         <Button title="Sign Out" onPress={signOut} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 

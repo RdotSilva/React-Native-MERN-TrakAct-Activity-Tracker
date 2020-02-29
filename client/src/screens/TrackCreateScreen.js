3b0db4987bce28cc6281bf5264c97e008929def1
RdotSilva <RyandotSilva@gmail.com>
2020-02-29 12:14:38 -0500

    Add error state

--- client/src/screens/TrackCreateScreen.js
+++ client/src/screens/TrackCreateScreen.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import Map from "./../components/Track/Map";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { requestPermissionsAsync } from "expo-location";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>

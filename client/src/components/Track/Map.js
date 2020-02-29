d5c243aeaa19151edf55039b1a43871fd378dbc1
RdotSilva <RyandotSilva@gmail.com>
2020-02-29 11:18:12 -0500

    Add initialRegion prop

--- client/src/components/Track/Map.js
+++ client/src/components/Track/Map.js
@@ -3,7 +3,17 @@ import { View, Text, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 
 const Map = () => {
-  return <MapView style={styles.map} />;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({

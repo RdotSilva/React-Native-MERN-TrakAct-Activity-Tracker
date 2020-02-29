21e30890318fd33c49b0f7316a35c2715b8fe43e
RdotSilva <RyandotSilva@gmail.com>
2020-02-29 11:13:19 -0500

    Add map styles

--- client/src/components/Track/Map.js
+++ client/src/components/Track/Map.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 const Map = () => {
-  return (
-    <View>
-      <Text>MAP</Text>
-    </View>
-  );
+  return <MapView style={styles.map} />;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default Map;

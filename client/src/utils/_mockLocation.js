ea0797f347e28da4a92b30f3d70a397b46b227ae
RdotSilva <RyandotSilva@gmail.com>
2020-03-01 11:56:52 -0500

    Update getLocation coords

--- client/src/utils/_mockLocation.js
+++ client/src/utils/_mockLocation.js
@@ -13,8 +13,8 @@ const getLocation = increment => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees
+      longitude: 41.8164572 + increment * tenMetersWithDegrees,
+      latitude: -71.4325848 + increment * tenMetersWithDegrees
     }
   };
 };

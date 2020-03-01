52965a0c633895ad020f2a03df6b9778cc635cf7
RdotSilva <RyandotSilva@gmail.com>
2020-03-01 12:04:36 -0500

    Add locationReducer

---
+++ client/src/context/locationContext.js
@@ -0,0 +1,10 @@
+import createDataContext from "./createDataContext";
+
+const locationReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    default:
+      return state;
+  }
+};

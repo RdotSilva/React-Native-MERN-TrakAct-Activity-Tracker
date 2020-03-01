c674d665310d8a22fbd7454456beff9dfb22c8be
RdotSilva <RyandotSilva@gmail.com>
2020-03-01 16:39:28 -0500

    Update addLocation action with dispatch

--- client/src/context/locationContext.js
+++ client/src/context/locationContext.js
@@ -13,7 +13,9 @@ const startRecording = dispatch => () => {};
 
 const stopRecording = dispatch => () => {};
 
-const addLocation = dispatch => () => {};
+const addLocation = dispatch => location => {
+  dispatch({ type: "ADD_CURRENT_LOCATION", payload: location });
+};
 
 export const { Context, Provider } = createDataContext(
   locationReducer,

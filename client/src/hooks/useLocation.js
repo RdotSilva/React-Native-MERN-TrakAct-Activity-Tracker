b3250df5b572cc98d4b4beedf5529e6919d60c6b
RdotSilva <RyandotSilva@gmail.com>
2020-03-02 08:58:34 -0500

    Add startWatching method

--- client/src/hooks/useLocation.js
+++ client/src/hooks/useLocation.js
@@ -7,4 +7,27 @@ import {
 
 export default () => {
   const [error, setError] = useState(null);
+
+  // This will request location access permission
+  const startWatching = async () => {
+    try {
+      const { status } = await requestPermissionsAsync();
+      if (status === "granted") {
+        await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          },
+          location => {
+            addLocation(location);
+          }
+        );
+      } else {
+        throw new Error("Location permission not granted");
+      }
+    } catch (err) {
+      setError(err);
+    }
+  };
 };

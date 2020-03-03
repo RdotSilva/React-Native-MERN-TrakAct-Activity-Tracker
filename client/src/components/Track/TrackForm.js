d0cabd906db067550663bcd65db24f38c2d5ce06
RdotSilva <RyandotSilva@gmail.com>
2020-03-03 12:00:46 -0500

    Add fragment to return

--- client/src/components/Track/TrackForm.js
+++ client/src/components/Track/TrackForm.js
@@ -3,15 +3,16 @@ import { Input, Button } from "react-native-elements";
 import Spacer from "./../Spacer";
 
 const TrackForm = () => {
-  return;
-  <>
-    <Spacer>
-      <Input placeholder="Enter name" />
-    </Spacer>
-    <Spacer>
-      <Button title="Start Recording" />
-    </Spacer>
-  </>;
+  return (
+    <>
+      <Spacer>
+        <Input placeholder="Enter name" />
+      </Spacer>
+      <Spacer>
+        <Button title="Start Recording" />
+      </Spacer>
+    </>
+  );
 };
 
 export default TrackForm;

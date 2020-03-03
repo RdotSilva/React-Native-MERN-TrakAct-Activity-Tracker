c06aa204aefe7ecfeb3d846374a708924b03294b
RdotSilva <RyandotSilva@gmail.com>
2020-03-03 11:35:11 -0500

    Update ngrok URL

--- client/src/api/tracker.js
+++ client/src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Ngrok URL must be updated every 8 hours.
 export default axios.create({
-  baseURL: "http://adeea7f8.ngrok.io"
+  baseURL: "http://051208ae.ngrok.io"
 });

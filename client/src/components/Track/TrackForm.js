1456f72deef894c7b9b5bcdfd3d94b0d7a62c3fe
RdotSilva <RyandotSilva@gmail.com>
2020-03-04 08:49:47 -0500

    Add LocationContext

--- client/src/components/Track/TrackForm.js
+++ client/src/components/Track/TrackForm.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Input, Button } from "react-native-elements";
 import Spacer from "./../Spacer";
+import { Context as LocationContext } from "../../context/locationContext";
 
 const TrackForm = () => {
+  const { startRecording, stopRecording, changeName } = useContext(
+    LocationContext
+  );
   return (
     <>
       <Spacer>

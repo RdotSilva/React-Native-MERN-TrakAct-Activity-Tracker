5eec7a8eefad690048a6aa57a25cd3d11241cdb0
RdotSilva <RyandotSilva@gmail.com>
2020-03-05 09:25:01 -0500

    Add trackReducer

---
+++ client/src/context/trackContext.js
@@ -0,0 +1,9 @@
+import createDataContext from "./createDataContext";
+
+const trackReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    default:
+      return state;
+  }
+};

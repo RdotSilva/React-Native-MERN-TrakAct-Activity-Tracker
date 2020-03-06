eb7ed2f681a4a3e76708656a0f8b988f45da91a0
RdotSilva <RyandotSilva@gmail.com>
2020-03-06 14:23:30 -0500

    Add createTrack action

--- client/src/hooks/useSaveTrack.js
+++ client/src/hooks/useSaveTrack.js
@@ -1,3 +1,7 @@
 import { useContext } from "react";
 import { Context as TrackContext } from "../context/trackContext";
 import { Context as LocationContext } from "../context/locationContext";
+
+export default () => {
+  const { createTrack } = useContext(TrackContext);
+};

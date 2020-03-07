f5ae075b9b7a9e069c6c69f82f570a5fff1196b2
RdotSilva <RyandotSilva@gmail.com>
2020-03-07 13:25:08 -0500

    Update useEffect to fetch tracks

--- client/src/screens/TrackListScreen.js
+++ client/src/screens/TrackListScreen.js
@@ -3,9 +3,13 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import { Context as TrackContext } from "../context/trackContext";
 
 const TrackListScreen = ({ navigation }) => {
-  const { fetchTracks } = useContext(TrackContext);
+  const { state, fetchTracks } = useContext(TrackContext);
+
+  // This is used to track focus on the track list screen
+  // If this is focused it will call the fetch tracks actions
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {});
+    fetchTracks();
 
     return unsubscribe;
   }, [navigation]);

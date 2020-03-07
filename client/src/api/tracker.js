f269940d0c6b0686bb56a8c30a31a36af4b0bdf5
RdotSilva <RyandotSilva@gmail.com>
2020-03-07 12:56:50 -0500

    Update ngrok URL

--- client/src/api/tracker.js
+++ client/src/api/tracker.js
@@ -3,7 +3,7 @@ import { AsyncStorage } from "react-native";
 
 // Ngrok URL must be updated every 8 hours.
 const instance = axios.create({
-  baseURL: "http://2d8143af.ngrok.io"
+  baseURL: "http://f2d4ba9e.ngrok.io"
 });
 
 // Automatically set the authorization header if we have a token in async storage

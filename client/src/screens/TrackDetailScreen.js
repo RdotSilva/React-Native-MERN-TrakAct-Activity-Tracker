f51e57c014231d76afbe10a49ed9b50f11f5c64b
RdotSilva <RyandotSilva@gmail.com>
2020-03-08 19:04:23 -0400

    Get initial coords from first location

--- client/src/screens/TrackDetailScreen.js
+++ client/src/screens/TrackDetailScreen.js
@@ -13,6 +13,10 @@ const TrackDetailScreen = ({ route, navigation }) => {
   // Search for the correct track based on _id
   const track = state.find(singleTrack => singleTrack._id === _id);
 
+  // Represents the very first set of coordinations that are are tracking
+  // Using this to set the starting viewpoint in map
+  const initialCoords = track.locations[0].coords;
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <>

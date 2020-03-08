93dfdf0ece31ee66239bc57ea8fe9962bdde7b27
RdotSilva <RyandotSilva@gmail.com>
2020-03-08 17:54:34 -0400

    Add state from track context

--- client/src/screens/TrackDetailScreen.js
+++ client/src/screens/TrackDetailScreen.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Context as TrackContext } from "../context/trackContext";
 
 const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+
   // Get track ID from navigation params
   const _id = navigation.getParam("_id");
   return (

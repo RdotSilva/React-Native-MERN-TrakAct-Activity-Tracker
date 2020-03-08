7f91895a7c5f5ee2c6ee699aa234d7d053a99c9e
RdotSilva <RyandotSilva@gmail.com>
2020-03-08 17:53:48 -0400

    Get _id from navigation params

--- client/src/screens/TrackDetailScreen.js
+++ client/src/screens/TrackDetailScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  // Get track ID from navigation params
+  const _id = navigation.getParam("_id");
   return (
     <View>
       <Text>TrackDetailScreen</Text>

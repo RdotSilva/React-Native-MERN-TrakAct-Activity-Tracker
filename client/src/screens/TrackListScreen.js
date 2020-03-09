f506ed1b4209568d576e59b8230b52a341469146
RdotSilva <RyandotSilva@gmail.com>
2020-03-09 14:42:45 -0400

    Add SafeAreaView to track list screen

--- client/src/screens/TrackListScreen.js
+++ client/src/screens/TrackListScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from "react";
 import { Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import { ListItem } from "react-native-elements";
 import { Context as TrackContext } from "../context/trackContext";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const TrackListScreen = ({ navigation }) => {
   const { state, fetchTracks } = useContext(TrackContext);
@@ -16,24 +17,25 @@ const TrackListScreen = ({ navigation }) => {
   }, [navigation]);
 
   return (
-    <>
-      <Text>TrackListScreen</Text>
-      <FlatList
-        data={state}
-        keyExtractor={item => item._id}
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate("TrackDetail", { _id: item._id })
-              }
-            >
-              <ListItem chevron title={item.name} />
-            </TouchableOpacity>
-          );
-        }}
-      />
-    </>
+    <SafeAreaView>
+      <>
+        <FlatList
+          data={state}
+          keyExtractor={item => item._id}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.navigate("TrackDetail", { _id: item._id })
+                }
+              >
+                <ListItem chevron title={item.name} />
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </>
+    </SafeAreaView>
   );
 };
 

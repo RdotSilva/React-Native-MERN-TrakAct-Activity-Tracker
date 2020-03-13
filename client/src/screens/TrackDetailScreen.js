5a6be0937bd3640b69d37e17addc1836cc4fbd0e
RdotSilva <RyandotSilva@gmail.com>
2020-03-13 17:43:41 -0400

    Add renderTypeIcon method

--- client/src/screens/TrackDetailScreen.js
+++ client/src/screens/TrackDetailScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import { Context as TrackContext } from "../context/trackContext";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView, { Polyline } from "react-native-maps";
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const TrackDetailScreen = ({ route, navigation }) => {
   const { state } = useContext(TrackContext);
@@ -17,11 +18,29 @@ const TrackDetailScreen = ({ route, navigation }) => {
   // Using this to set the starting viewpoint in map
   const initialCoords = track.locations[0].coords;
 
+  // Used to render the icon based on activity type
+  const renderTypeIcon = type => {
+    switch (type) {
+      case "run":
+        return <MaterialCommunityIcons name="run" />;
+        break;
+      case "hike":
+        return <MaterialCommunityIcons name="pine-tree" />;
+        break;
+      case "bike":
+        return <MaterialCommunityIcons name="bike" />;
+        break;
+      case "other":
+        return <FontAwesome name="question" />;
+      default:
+        break;
+    }
+  };
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <>
         <Text style={styles.title}>{track.name}</Text>
-        <Text style={styles.type}>{track.type}</Text>
         <MapView
           style={styles.map}
           initialRegion={{
